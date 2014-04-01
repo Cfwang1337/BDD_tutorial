0a69692902987153b0ffbad3796476c234d930be
Charles <charles@Deth-Ubuntu-Box.(none)>
2014-03-31 22:56:42 -0400

    BDD tutorial completed

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    #meal_cost = request.form.submit
+    print request.form
+    meal_cost = request.form['meal_cost']
+    tip_percentage = request.form['tip_percentage']
+    tip_amount = abs(float(request.form['meal_cost']))*abs((float(request.form['tip_percentage'])/100))
+    result_dict = dict(
+        meal_cost = meal_cost,
+        tip_percentage = tip_percentage,
+        tip_amount = tip_amount        
+        )
+    return render_template('results.html', data=result_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,7 @@
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
+
+class CalculatorForm(Form):
+    meal_cost = TextField('meal_cost', validators = [Required()])
+    tip_percentage = TextField('tip_percentage', validators = [Required()])
